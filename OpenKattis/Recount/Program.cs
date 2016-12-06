3ebb90ea14e5bb2ecf47190917962e316b302c76
mfathirirhas <fatheer.eerhas@gmail.com>
2016-12-06 07:20:08 +0700

    Do Recount Problem

---
+++ OpenKattis/Recount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			  	string input;
+			  	List<string> list = new List<string>();
+         	while((input = Console.ReadLine()) != "***"){
+					list.Add(input);
+				}
+        }
+    }
+}
