9b6148e171ef88f55be7480f51b7ae0617cf0e2a
mfathirirhas <fathir.irhas@outlook.com>
2017-01-13 17:43:48 +0700

    On Progress cd.py openkattis

--- OpenKattis/cd.py
+++ OpenKattis/cd.py
@@ -1,6 +1,35 @@
 # Author: Muhammad Fathir Irhas - 30/11/2016 - OpenKattis
-# https://github.com/fabianschilling/kattis/blob/master/cd/CD.java
 
+mm = 1000000
+n_list = []
+m_list = []
 
+while(True):
+    n,m = map(int, raw_input().split())
 
+    if(n == 0 and m == 0):
+        break
+
+    for i in range(0,n):
+        n_input = input()
+        n_list.append(n_input)
+
+    for j in range(0,m):
+        m_input = input()
+        m_list.append(m_input)
+
+    sold = 0
+    ni = 0
+    mi = 0
+    while ni < n and mi < m:
+        if(n_list[ni] > m_list[mi]):
+            mi+=1
+        elif(n_list[ni] < m_list[mi]):
+            ni+=1
+        else:
+            sold+=1
+            ni+=1
+            mi+=1
+    
+    print sold
 
