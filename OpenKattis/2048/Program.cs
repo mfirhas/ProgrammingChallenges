2f2ae6cc338989f01e1936c1473f892da8acacb8
mfathirirhas <fathir.irhas@outlook.com>
2017-01-17 12:17:51 +0700

    on progress 2048 openkattis

--- OpenKattis/2048/Program.cs
+++ OpenKattis/2048/Program.cs
@@ -10,6 +10,15 @@ namespace _048
         // Movement
         // 0:left, 1:up, 2:right, 3:down
 
+        public static void Display(int[][] fState){
+            for(int i=0;i<4;i++){
+                for(int j=0;j<4;j++){
+                    Console.WriteLine(fState[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
         // 0 -> Moveleft
         public static int[][] MoveLeft(int[][] initialState){
             int[][] initState = initialState;
@@ -17,24 +26,52 @@ namespace _048
 
             for(int i=0;i<4;i++){
                 for(int j=0;j<4;j++){
-                    if(initState[i][j]!=0 && initState[i][j] == initState[i][j+1]){
-                        finalState[i][j] += initState[i][j] + initState[i][j+1];
-                    }else if(initState[i][j]!=0 && initState[i][j+1] == 0){
-                        int k = j+1;
-                        while(k<4 && initState[i][k]==0){
-                            if(initState[i][j] == initState[i][k]){
-                                finalState[i][j] = initState[i][j] + initState[i][k];
-                            }
-                            k++;
+                    int k = j+1;
+                    while(initState[i][k] == 0 && k<4){
+                        if(initState[i][j] == initState[i][k]){
+                            finalState[i][j] = initState[i][j] + initState[i][k];
+                            initState[i][k] = 0;
+                        }else if(initState[i][j] == 0 && initState[i][k]!=0){
+                            finalState[i][j] = initState[i][j] + initState[i][k];
+                            initState[i][k] = 0;
+                        }else if(initState[i][j] != 0 && initState[i][j]!=initState[i][k]){
+                            break;
+                        }else if(k == 3){
+                            break;
                         }
+                        k++;
                     }
                 }
             }
-            
+            return finalState;
         }
 
         public static void Main(string[] args)
         {
+            string[] firstLine = Console.ReadLine().Split();
+            string[] secondLine = Console.ReadLine().Split();
+            string[] thirdLine = Console.ReadLine().Split();
+            string[] fourthLine = Console.ReadLine().Split();
+
+            int[][] iState = new int[4][];
+            for(int i=0;i<4;i++){
+                iState[0][i] = int.Parse(firstLine[i]);
+            }
+
+            for(int i=0;i<4;i++){
+                iState[1][i] = int.Parse(secondLine[i]);
+            }
+
+            for(int i=0;i<4;i++){
+                iState[2][i] = int.Parse(thirdLine[i]);
+            }
+
+            for(int i=0;i<4;i++){
+                iState[3][i] = int.Parse(fourthLine[i]);
+            }
+
+            int[][] fState = MoveLeft(iState);
+            Display(fState);
 
         }
     }
