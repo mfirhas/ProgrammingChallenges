6608a81d84456b779997562386d02a6465701006
mfathirirhas <fathir.irhas@outlook.com>
2017-01-18 17:48:58 +0700

    on progress Statistics_CS

---
+++ OpenKattis/Statistics_CS/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Statistics_CS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
