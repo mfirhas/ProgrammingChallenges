b1e6b3eb8316910c92a1a49e503f4d412a9bc5c4
mfathirirhas <fatheer.eerhas@gmail.com>
2017-01-19 23:51:26 +0700

    solved project euler 11 hackerrank

--- Hackerrank/ProjectEuler_Hackerrank/011.cpp
+++ Hackerrank/ProjectEuler_Hackerrank/011.cpp
@@ -2,23 +2,22 @@
 
 using namespace std;
 
-
+const int N=20;
+int m[N+10][N+10];
 
 int main(){
-    int m[20][20];
-
-    for(int i=0;i<20;i++){
-        for(int j=0;j<20;j++){
-            cin >> m[i][j];
+    int max_product=0;
+    for(int i=3;i<N+3;++i)
+        for(int j=3;j<N+3;++j)
+            cin>>m[i][j];
+    for(int i=3;i<N+3;++i)
+        for(int j=3;j<N+3;++j)
+        {
+            max_product=max(max_product,m[i][j]*m[i+1][j]*m[i+2][j]*m[i+3][j]);
+            max_product=max(max_product,m[i][j]*m[i][j+1]*m[i][j+2]*m[i][j+3]);
+            max_product=max(max_product,m[i][j]*m[i+1][j+1]*m[i+2][j+2]*m[i+3][j+3]);
+            max_product=max(max_product,m[i][j]*m[i-1][j+1]*m[i-2][j+2]*m[i-3][j+3]);
         }
-    }
-
-    vector<int> lists;
-    lists.push_back(Horizontal(m));
-    lists.push_back(Vertical(m));
-    lists.push_back(Diagonal(m));
-    int max = *max_element(lists.begin(), lists.end());
-    cout << max << endl;
-
-    return 0;
+     cout<<max_product<<endl;
+     return  0;
 }
