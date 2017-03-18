8ee5c2ad067a384729079b2add5e95c8b15cf1c7
mfathirirhas <fatheer.eerhas@gmail.com>
2017-03-19 01:25:13 +0700

    on progress ptice.cpp

--- OpenKattis/ptice.cpp
+++ OpenKattis/ptice.cpp
@@ -5,25 +5,39 @@
 using namespace std;
 
 void foo(int num, string input){
-	string names = {"Adrian", "Bruno", "Goran"};
-	char answers[][] = {{'A','B','C'}, {'B', 'A', 'B', 'C'}, {'C', 'C', 'A', 'A', 'B', 'B'}};
+	string names[] = {"Adrian", "Bruno", "Goran"};
+	// char answers[][6] = {{'A','B','C'}, {'B', 'A', 'B', 'C'}, {'C', 'C', 'A', 'A', 'B', 'B'}};
+	vector< vector<char> > answers = {
+		{'A','B','C'},
+		{'B', 'A', 'B', 'C'},
+		{'C', 'C', 'A', 'A', 'B', 'B'}
+	};
 	int scores[3] = {0,0,0};
 
 	for(int i=0;i<input.length();i++){
 		for(int j=0;j<3;j++){
-			if(input[i] == answers[j][i % (sizeof(answers[j])/sizeof(answers[j][0]))]){
+			if(input[i] == answers[j][i % answers[j].size()){
 				scores[j] += 1;
 			}
 		}
 	}
 
-	int highest_score = *std::max_element(scores);
+	std::vector<int> v(scores, scores + sizeof scores / sizeof scores[0]);
+	int highest_score = *std::max_element(v.begin(), v.end());
 	cout << highest_score << endl;
-	for(int i=0;i<3;i++){
-		if(scores[i] == highest_score){
-			cout << names[i] << endl;
-		}
-	}
+	(scores[0] == highest_score) ? cout<<"Adrian"<< endl : cout<<"";
+	(scores[1] == highest_score) ? cout<<"Bruno"<< endl : cout<<"";
+	(scores[2] == highest_score) ? cout<<"Goran"<< endl : cout<<"";
+
+	cout << "Array length " << sizeof(answers[0])/sizeof(answers[0][0]);
+	cout << scores[0] << endl;
+	cout << scores[1] << endl;
+	cout << scores[2] << endl;
+	// for(int i=0;i<3;i++){
+	// 	if(scores[i] == highest_score){
+	// 		cout << names[i] << "\n";
+	// 	}
+	// }
 }
 
 int main(){
