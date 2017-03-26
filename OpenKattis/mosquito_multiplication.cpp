ff98a517fcd936c0e2b1a4d87f4206989e570712
mfathirirhas <fatheer.eerhas@gmail.com>
2017-03-26 19:03:53 +0700

    on progress mosquito_multiplication.cpp openkattis

---
+++ OpenKattis/mosquito_multiplication.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Author: Muhammad Fathir Irhas - OpenKattis - 26/03/2017
+ */
+
+int SurvivedMosquitos(int M , int P, int L, int E, int R, int S, int N){
+    int m = M;
+    int p = P;
+    int l = L;
+
+    for(int i=0;i<N;i++){
+        int m_ = floor(p/S);
+        int p_ = floor(l/R);
+        int l_ = m * E;
+
+        m = m_;
+        p = p_;
+        l = l_;
+    }
+
+    return m;
+}
+
+int main(){
+    int M , P, L, E, R, S, N;
+    cin >> M >> P >> L >> E >> R >> S >> N;
+    cout << SurvivedMosquitos(M , P, L, E, R, S, N) << endl;
+    return 0;
+}
