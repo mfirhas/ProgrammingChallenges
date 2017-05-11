cf6b3aee5028d626e009c6ed077912228ab5ccee
mfathirirhas <fatheer.eerhas@gmail.com>
2017-05-12 01:23:32 +0700

    solved compound_words.py openkattis

--- OpenKattis/compound_words.py
+++ OpenKattis/compound_words.py
@@ -1,14 +1,16 @@
 from sys import stdin
 
+# author: Muhammad Fathir Irhas - 12/05/2017 - OpenKattis
+
 def Permute(l):
     permuted = []
     for i in range(len(l)):
-        d = l
-        x = del d[i]
-        for j in range(len(x)):
-            permuted.append(l[i]+x[j])
-    list(set(permuted))
-    sorted(permuted)
+        d = l[:]
+        del d[i]
+        for j in range(len(d)):
+            permuted.append(l[i]+d[j])
+    permuted = list(set(permuted))
+    permuted = sorted(permuted, key=str.lower)
     return permuted
 
 l = []
@@ -19,5 +21,7 @@ while True:
         for i in range(len(y)):
             l.append(y[i])
     except(EOFError):
-        print(Permute(l))
+        p = Permute(l)
+        for i in range(len(p)):
+            print(p[i])
         break
