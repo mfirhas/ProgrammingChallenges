fbaffbb5f76d51ab762c4e7f059b8d55e9362ae5
mfathirirhas <fathir.irhas@outlook.com>
2017-07-11 17:32:46 +0700

    on progress welcome to code jam easy

---
+++ OpenKattis/WelcomToCodeJam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WelcomToCodeJam
+{
+    class Program
+    {
+        static int CountText(string input, int indice=0){
+            string Text = "welcome to code jam";
+            int lenText = Text.Length;
+            int count = 0;
+
+            if(indice == lenText-1){
+                if(input[indice] == 'm'){
+                    count += 1;
+                }
+            }
+
+            
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
