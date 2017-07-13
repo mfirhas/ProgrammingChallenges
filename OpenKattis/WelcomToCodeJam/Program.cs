97f1fe65e125391ad2bc256e476f4665c7e91e1b
mfathirirhas <fathir.irhas@outlook.com>
2017-07-13 12:45:22 +0700

    on progress WelcomeToCodeJam(Easy) - OpenKattis

--- OpenKattis/WelcomToCodeJam/Program.cs
+++ OpenKattis/WelcomToCodeJam/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WelcomToCodeJam
 {
     class Program
     {
         static int CountText(string input, int indice=0){
+
             string Text = "welcome to code jam";
             int lenText = Text.Length;
             int count = 0;
 
-            if(indice == lenText-1){
-                if(input[indice] == 'm'){
-                    count += 1;
+            if(indice == lenText - 1){
+                foreach(char i in input){
+                    if(i == 'm')
+                        count++;
                 }
+                return count;
             }
 
             
