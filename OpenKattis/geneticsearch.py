027eead5165e1201be7080d5b805d662544f21ee
mfathirirhas <fathir.irhas@outlook.com>
2017-09-05 15:10:31 +0700

    on progress type3 geneticsearch openkattis

--- OpenKattis/geneticsearch.py
+++ OpenKattis/geneticsearch.py
@@ -1,5 +1,3 @@
-
-
 class GeneticSearch:
 
     global type1
@@ -12,16 +10,32 @@ class GeneticSearch:
 
     def CountType1(self):
         type1 = 0
-        for i in range((len(L)-len(S))+1):
-            temp = L[i:i+len(S)]
-            if(S == temp):
+        for i in range((len(self.L)-len(self.S))+1):
+            temp = self.L[i:i+len(self.S)]
+            if(self.S == temp):
                 type1 += 1
         
         return type1
 
     def CountType2(self):
         type2 = 0
+        l = self.unique_removed_strings(self.S)
+        for i in range(len(l)):
+            for j in range((len(self.L)-len(l[i]))+1):
+                temp = self.L[j:j+len(l[i])]
+                if(l[i] == temp):
+                    type2 +=1
+
+        return type2
         
+    def remove_one_char(self, sl, i):
+        return sl[:i] + sl[i+1:]
+
+    def unique_removed_strings(self, S):
+        l = []
+        for i in range(len(S)):
+            l.append(self.remove_one_char(S, i))
+        return list(set(l))
 
 
 if __name__ == '__main__':
@@ -29,10 +43,12 @@ if __name__ == '__main__':
     while(sl!='0'):
         S, L = sl.split()
         search = GeneticSearch(S,L)
-        print(search.CountType1())
+        print(search.CountType1(), search.CountType2())
         sl = input()
         if(sl=='0'):
             break
-    
+    # c = GeneticSearch()
+    # l = c.unique_removed_strings(sl)
+    # print(l)
 
         
