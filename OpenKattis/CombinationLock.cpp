8d6df94d730568e711c660ed9803cf3324aaed99
mfathirirhas <fathir.irhas@outlook.com>
2017-11-11 22:27:59 +0700

    on progress combinaiton lock openkattis

---
+++ OpenKattis/CombinationLock.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int x,y,z;
+    // cin >> x >> y >> z;
+    // cout << x << y << z;
+    cout << 360+360+270+360+270+270;
+    return 0;
+}
